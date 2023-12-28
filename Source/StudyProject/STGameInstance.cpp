c796fda1147532f1cda08b37931210b76c4f09dd
briankunkel9 <briankunkel9@gmail.com>
2023-12-28 22:01:30 +0900

    update game instance
    update game instance

--- Source/StudyProject/STGameInstance.cpp
+++ Source/StudyProject/STGameInstance.cpp
@@ -6,11 +6,22 @@
 
 USTGameInstance::USTGameInstance()
 {
+	FString CharacterDataPath = TEXT("/Game/Book/GameData/STCharacterData.STCharacterData");
+	static ConstructorHelpers::FObjectFinder<UDataTable> DT_STCharacter(*CharacterDataPath);
 
+	STCHECK(DT_STCharacter.Succeeded());
+	STCharacterTable = DT_STCharacter.Object;
+	STCHECK(STCharacterTable->GetRowMap().Num() > 0);
 }
 
 void USTGameInstance::Init()
 {
 	Super::Init();
-	STLOG_S(Warning);
+
+	ST_LOG(Warning, TEXT("DropExp of Level 20 STCharcter : %d"), GetSTCharacterData(20)->DropExp );
+}
+
+FSTCharacterData* USTGameInstance::GetSTCharacterData(int32 Level)
+{
+	return STCharacterTable->FindRow<FSTCharacterData>(*FString::FromInt(Level),TEXT(""));
 }
